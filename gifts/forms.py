5ffccb47ccc237e68ed8ada63952e1216cbf0897
slawek <slawomirpawlowski@gmail.com>
2019-12-19 23:17:36 +0100

    updated templates and forms

--- gifts/forms.py
+++ gifts/forms.py
@@ -1,7 +1,13 @@
 from django.forms import ModelForm
+from django.utils.translation import gettext_lazy as _
+
 from .models import GiftsList
+
 
 class GiftsListForm(ModelForm):
 	class Meta:
 		model = GiftsList
 		fields = ['name']
+		labels = {
+            'name': _(''),
+        }    
