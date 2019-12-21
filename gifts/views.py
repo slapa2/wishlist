9820aed2c94c3577d3a9f16e93440d5ac64f6570
slawek <slawomirpawlowski@gmail.com>
2019-12-21 20:44:13 +0100

    code cleaned

--- gifts/views.py
+++ gifts/views.py
@@ -3,10 +3,8 @@ from django.shortcuts import render
 from .models import GiftsList, Gift
 from .forms import GiftsListForm
 
-# Create your views here.
-def get_gifts_list(request):
 
-	# form to create new gifts list
+def get_gifts_list(request):
 	if request.method == 'POST':
 		form = GiftsListForm(request.POST)
 		if form.is_valid():
@@ -14,9 +12,7 @@ def get_gifts_list(request):
 	else:
 		form = GiftsListForm()
 	
-	# lists of gifts list
 	queryset = GiftsList.objects.all
-	
 	context = {
 		'form': form,
 		'queryset': queryset 
@@ -24,9 +20,7 @@ def get_gifts_list(request):
 	return render(request, 'gifts/gifts_list.html', context)
 
 def get_gift(request, id):
-
 	queryset = Gift.objects.filter(gift_list=id)
-
 	context = {
 		'queryset': queryset
 	}
