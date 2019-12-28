3d7a8f2f7597d65a8d2ee87612bda6fc97c6506b
slawek <slawomirpawlowski@gmail.com>
2019-12-28 15:22:56 +0100

    modified view inport in urls.py

--- gifts/urls.py
+++ gifts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from . import views
+from .views import (
+    index,
+)
 
 urlpatterns = [
-    path('', views.index, name='landing_page'),
+    path('', index, name='landing_page'),
 ]
