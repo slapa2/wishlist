2392cf2a9e0d69e761838b5397d8a25230a85b96
slawek <slawomirpawlowski@gmail.com>
2020-01-11 13:24:33 +0100

    added redirect_authenticated_user=True win login view

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -28,6 +28,6 @@ urlpatterns = [
     path('user/', include('users.urls')),
     
     path('register/', register_user_view, name='register'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('login/', auth_views.LoginView.as_view(redirect_authenticated_user=True ), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
 ]
