84405712753640c60c2bb2b91e4d0f5a69bfb99d
slawek <slawomirpawlowski@gmail.com>
2020-01-11 13:44:09 +0100

    code cleaned

--- users/views.py
+++ users/views.py
@@ -1,9 +1,6 @@
 from slugify import slugify
 
 from django.shortcuts import render, redirect
-
-
-from django.contrib.auth import login, authenticate
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 
