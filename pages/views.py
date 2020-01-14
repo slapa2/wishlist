df37c4231df8484a36e12b40fbb7f68fa254d836
slawek <slawomirpawlowski@gmail.com>
2020-01-14 19:45:16 +0100

    website rebranding

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def landing_page(request):
+
+    if request.user.is_authenticated:
+        return redirect('user_home_page_view')
+
     template = 'pages/landing_page.html'
     context = {}
     return render(request, template, context)
--- simple_list/urls.py
+++ simple_list/urls.py
@@ -1,4 +1,4 @@
-"""wishlist URL Configuration
+"""simple_list URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
